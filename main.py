c3ecbc50df08c1b1be7ed11e547d03b5eef47291
abhishek9822 <sharmaabhishek9822@gmai.com>
2024-11-12 14:01:51 +0530

    main file

---
+++ main.py
@@ -0,0 +1,6 @@
+from gender.training_pipeline.training_pipeline import TrainingPipeline
+
+
+if __name__ == "__main__":
+    training_pipeline = TrainingPipeline()
+    training_pipeline.run_pipeline()
